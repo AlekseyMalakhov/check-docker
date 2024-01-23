f7f4e38a07579e6a788f08cf0053f43a1f68f0ed
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-01-23 17:43:21 +0200

    check

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Item 123'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
